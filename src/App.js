cef98471c67a38d8faa1bd43aa88a23fbcb32104
DnlGalvan <84108325+DnlGalvan@users.noreply.github.com>
2021-05-29 16:10:58 -0300

    V_1.5

--- src/App.js
+++ src/App.js
@@ -24,25 +24,64 @@ function App() {
   }
 
   return (
-    <div>
-      <input type="text" id="cidade" onKeyPress={checarEnter}/>
-      <button onClick={buscarPrevisao}>Previsão</button>
-      {
-        tempo && (
-        <>
-          <p>Previsão do tempo</p>
-          <p>Cidade: {tempo.name}</p>
-          <p>Pais: {tempo.sys.country}</p>
-          <p>Clima: {tempo.weather[0].main} - {tempo.weather[0].description}</p>
-          <p>Temperatura: {tempo.main.temp}</p>
-          <p>Sensação Térmica: {tempo.main.feels_like}</p>
-          <p>Temperatura mínima: {tempo.main.temp_min}</p>
-          <p>Temperatura máxima: {tempo.main.temp_max}º</p>
-          <p>Vento: {tempo.wind.speed}</p>
-        </>
-        )
-      }
-    </div>
+    <main>
+      <div id="cabecalho">
+        <h1>CLIMAPREV</h1>
+      </div>
+      <div id="cabecalho_bottom">
+        <h3>Clima e Previsão do Tempo</h3>
+      </div>
+      
+      <div id="selecao">
+        <select name="estados-brasil">
+          <option value="UF">Selecione um Estado</option>
+          <option value="AC">Acre</option>
+          <option value="AL">Alagoas</option>
+          <option value="AP">Amapá</option>
+          <option value="AM">Amazonas</option>
+          <option value="BA">Bahia</option>
+          <option value="CE">Ceará</option>
+          <option value="DF">Distrito Federal</option>
+          <option value="ES">Espírito Santo</option>
+          <option value="GO">Goiás</option>
+          <option value="MA">Maranhão</option>
+          <option value="MT">Mato Grosso</option>
+          <option value="MS">Mato Grosso do Sul</option>
+          <option value="MG">Minas Gerais</option>
+          <option value="PA">Pará</option>
+          <option value="PB">Paraíba</option>
+          <option value="PR">Paraná</option>
+          <option value="PE">Pernambuco</option>
+          <option value="PI">Piauí</option>
+          <option value="RJ">Rio de Jeneiro</option>
+          <option value="RN">Rio Grande do Norte</option>
+          <option value="RS">Rio Grande do Sul</option>
+          <option value="RO">Rondônia</option>
+          <option value="RR">Roraima</option>
+          <option value="SC">Santa Catarina</option>
+          <option value="SP">São Paulo</option>
+          <option value="SE">Sergipe</option>
+          <option value="TO">Tocantins</option>
+        </select>
+        <input type="text" id="cidade" placeholder="Cidade" onKeyPress={checarEnter}/>
+        <button onClick={buscarPrevisao}>Previsão</button>
+        {
+          tempo && (
+          <>
+            <p id="titulo">Previsão do tempo</p>
+            <p>Cidade: {tempo.name}</p>
+            <p>Pais: {tempo.sys.country}</p>
+            <p>Clima: {tempo.weather[0].main} - {tempo.weather[0].description}</p>
+            <p>Temperatura: {tempo.main.temp}ºC</p>
+            <p>Sensação Térmica: {tempo.main.feels_like}ºC</p>
+            <p>Temperatura mínima: {tempo.main.temp_min}ºC</p>
+            <p>Temperatura máxima: {tempo.main.temp_max}ºC</p>
+            <p>Velocidade Vento: {tempo.wind.speed}</p>
+          </>
+          )
+        }
+      </div>
+    </main>
   );
 }
 
